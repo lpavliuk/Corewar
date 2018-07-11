bbc51c1d7572418c7598a02504fc6fdc47d79fe8
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-07-11 16:00:43 +0300

    e

--- vm_champs/champs/ex.s
+++ vm_champs/champs/ex.s
@@ -1,7 +1,16 @@
-.name "zork"
-.comment "just a basic living prog"
-		
-l2:	sti	r1,%:live,%0
-	and	r1,%0,r1
-live:	live	%1
-	zjmp	%:live
+
+.comment      "just a basic living prog" 
+
+.name "asdkasd"
+
+live:	sti	r1,%:hui,%0
+	
+
+live:#askad;lksa
+and	r1,%0,r1
+
+
+asdkasd:
+asas:
+live:live	%1
+hui: zjmp	%:live
