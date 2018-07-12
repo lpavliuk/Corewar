c57f75772df11bd2872a3104cdfa5d693cb1a5ed
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-07-12 15:59:36 +0300

    input binary in file

--- ASM/create_binary.c
+++ ASM/create_binary.c
@@ -36,14 +36,27 @@ int		main(void)
 	char *name = "name.cor";
 	unsigned int	magic = COREWAR_EXEC_MAGIC;
 	unsigned int	rew_magic;
+	unsigned int	rew_bot_size;
+	char 			prog_name[PROG_NAME_LENGTH + 1];
+	char			comment[COMMENT_LENGTH + 1];
+	char			null[4];
+	unsigned int	bot_size = 15;
 
+	ft_bzero(prog_name, PROG_NAME_LENGTH + 1);
+	ft_bzero(comment, COMMENT_LENGTH + 1);
+	ft_bzero(null, 4);
+	ft_strcat(prog_name, "name");
+	ft_strcat(comment, "comment");
 	rew_magic = reverse_bytes(magic);
-	printf("%u\n", magic);
-	printf("%u\n", rew_magic);
+	rew_bot_size = reverse_bytes(bot_size);
 	fd = open(name, O_RDWR | O_TRUNC | O_CREAT, 0777);
 
 	write(fd, &rew_magic, 4);
-	write(fd, &magic, 4);
+	write(fd, &prog_name, PROG_NAME_LENGTH);
+	write(fd, &null, 4);
+	write(fd, &rew_bot_size, 4);
+	write(fd, &comment, COMMENT_LENGTH);
+	write(fd, &null, 4);
 
 	close(fd);
 	return (0);
