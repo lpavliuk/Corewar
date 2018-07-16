30142a23245b377524893f46026a89fd80ed454d
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-07-16 17:40:56 +0300

    flags -a and create binary is done

--- vm_champs/ex.s
+++ vm_champs/ex.s
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-.name "name"
-
-.comment "comment"
-
-l2:	sti	r1,%:live,%0
-live:	live	%1
-	zjmp	%:l2
-=======
 .name""
 .comment""
-<<<<<<< HEAD
 
 lld %:live,r1
->>>>>>> 01a93003f8651be4a0727ea3073a1f5f5166ecd3
-=======
 ;doufghdh
->>>>>>> 84e5e851ca1c0fa134b6f49f7f2c1f35ffd969d2
