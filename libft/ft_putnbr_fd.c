d4a7eee6836c91721ed23c6f8c68e6c8f058aab1
tataqwerty <tarasik2000@gmail.com>
2018-07-17 15:37:24 +0300

    changed libft

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,17 +6,22 @@
 /*   By: opavliuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 16:00:09 by opavliuk          #+#    #+#             */
-/*   Updated: 2018/03/26 16:03:36 by opavliuk         ###   ########.fr       */
+/*   Updated: 2018/07/17 15:36:18 by tkiselev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(long n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
+		if (n == -2147483648)
+		{
+			ft_putchar_fd('2', fd);
+			n /= 10;
+		}
 		n = n * (-1);
 	}
 	if (n >= 10)
