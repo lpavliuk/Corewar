1126aa5e663b309602dda6ab8dfec206f670946e
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-07-23 19:47:14 +0300

    the rock

--- vm_champs/therock.s
+++ vm_champs/therock.s
@@ -1,11 +1,11 @@
 .name "The Rock"
 .comment "I will kick your ass!"
 
-		zjmp  %120
 name:	sti r1,%:live,%1
 
 live:	live %1
-
+		zjmp %:live
+load:
 		ldi %:live,%1,r15
 		ld %16777216,r2 #  saves 01 00 00 00 to r2
 		ld %150,r3
@@ -13,16 +13,36 @@ live:	live %1
 		ld %140,r6
 		ld %6,r5
 		ld %589944,r10
-		# ld %260308992,r3 # saves 0f 84 00 00 to r3
-		# ld %191365632,r4
-		# ld %38411,r5
-		# ld %1745813504,r6
-		# ld %9440104,r7
-		# ld %67108864,r8
-		# ld %2366334981,r9
-		# ld %138,r10
-		# ld %191366656,r11
+		ld %0,r11
+		ld %0,r12
+		ld %100,r13
+
+		and   r1,%0,r14
 		zjmp  %:forkin
+
+zero0: 	sti   r15,%0,%8
+		live %1
+		sti   r11,%250,r12
+		add   r12,r13,r12
+
+		and   r1,%0,r14
+		zjmp  %:zero0
+
+zero1:  sti   r15,%0,%8
+		live %1
+		sti   r11,%500,r12
+		add   r12,r13,r12
+
+		and   r1,%0,r14
+		zjmp  %:zero1
+
+zero2:  sti   r15,%0,%8 
+		live %1
+		sti   r11,%750,r12
+		add   r12,r13,r12
+
+		and   r1,%0,r14
+		zjmp  %:zero2
 
 write:
 		sti   r2,%0,r3 #  write 01 00 00 00
@@ -33,15 +53,16 @@ write:
 		add   r4,r5,r4
 		add   r6,r5,r6
 
-		and r1,%0,r1
+		and   r1,%0,r14
 		zjmp  %:write
 
 forkin:
 		fork  %:forkin
+		fork  %:name
+		fork  %:zero0
+		fork  %:zero1
+		fork  %:zero2
 
-		fork  %:write
-
-		zjmp  %100
 
 		# sti   r3,%0,%141 #  write command 0f and dir 84 from r3
 		# sti   r4,%0,%141 # copys comands to copy!
