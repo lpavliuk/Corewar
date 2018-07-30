1fcbd24dd1326b87fd806284cf6a515d31fa0d0e
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-07-30 20:51:35 +0300

    test socket

---
+++ VM/test_socket.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_socket.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opavliuk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/30 19:38:09 by opavliuk          #+#    #+#             */
+/*   Updated: 2018/07/30 19:38:10 by opavliuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+int		main(int argc, char **argv)
+{
+
+	return (0);	
+}
