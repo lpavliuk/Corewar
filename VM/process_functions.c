77c88634c90ba8288f3d3ebda6167df69899c646
Yevhen Samchuk <evsamchuk@gmail.com>
2018-07-31 16:31:46 +0300

    ft_ld

--- VM/process_functions.c
+++ VM/process_functions.c
@@ -1,5 +1,30 @@
 #include "corewar.h"
 
+int		check_valid_codage(char opcode, char *codage)
+{
+	char	input_arg;
+	char	possible_arg;
+	char	it_was;
+
+	input_arg = 0;
+	while(input_arg < COUNT_ARGS(opcode))
+	{
+		possible_arg = 0;
+		it_was = 0;
+		while(possible_arg < 3)
+		{
+			if (ARG(opcode, input_arg, possible_arg)
+				&& codage[input_arg] == possible_arg + 1)
+				it_was = 1;
+			possible_arg++;
+		}
+		if (!it_was)
+			return (0);
+		input_arg++;
+	}
+	return (1);
+}
+
 void	ft_live(t_process *process, t_vm *vm)
 {
 	process->live = 1;
@@ -13,26 +38,31 @@ void	ft_live(t_process *process, t_vm *vm)
 		g_pixels[process->position]->live = 50;
 }
 
+void	change_process_position_by_codage()
+{
+
+}
+
 void	ft_ld(t_process *process, t_vm *vm)
 {
 	char			codage[4];
-	unsigned int	ar1;
-	unsigned int	ar2;
+	unsigned int	arg;
 	unsigned int	result;
 
 	decipher_codage(codage, COUNT_ARGS(2), (process->position + 1) % MEM_SIZE);
-	if (codage[0] == IND_CODE)
+	if (check_valid_codage(OPCODE(1), codage) && )
 	{
-		ar1 = get_arg((process->position + 2) % MEM_SIZE, T_IND_SIZE) % IDX_MOD;
-		result = get_arg((process->position + ar1) % MEM_SIZE, T_IND_READ);
-		ar2 = get_arg((process->position + 4) % MEM_SIZE, T_REG_SIZE);
+		result = get_arg((process->position + ((codage[0] == IND_CODE)
+			? (get_arg((process->position + (get_arg((process->position + 2)
+			% MEM_SIZE, T_IND_SIZE) % IDX_MOD)) % MEM_SIZE, T_IND_READ)) : 2))
+			% MEM_SIZE, T_DIR_SIZE);
+		arg = get_arg((process->position + ((codage[0] == IND_CODE) ? 2 : 4))
+			% MEM_SIZE, T_REG_SIZE);
+		process->registries[arg] = result;
+		process->position = ((codage[0] == IND_CODE) ? 2 : 4) + 3; /* offset is (codage + arg1(T_IND || T_DIR) + arg2 + 1) */
 	}
 	else
-	{
-		result = get_arg((process->position + 2) % MEM_SIZE, T_DIR_SIZE);
-		ar2 = get_arg((process->position + 3) % MEM_SIZE, T_REG_SIZE);
-	}
-	process->registries[ar2] = result;
+		change_process_position_at_wrong_codage();
 }
 
 // void	ft_st(t_process *process, t_vm *vm)
