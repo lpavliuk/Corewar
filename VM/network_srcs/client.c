08e725028e730964f73c53e779b999da47a0aa97
tataqwerty <tarasik2000@gmail.com>
2018-08-06 15:07:11 +0300

    bugs fixed

--- VM/network_srcs/client.c
+++ VM/network_srcs/client.c
@@ -59,8 +59,8 @@ static void				read_init_info(int socket_fd, fd_set read_fds)
 		{
 			sleep(1);
 			system("clear");
-			printf("Time to start the game: %d\n", str[0]);
-			printf("Number of connected players: %d\n", str[1]);
+			ft_printf("Time to start the game: %d\n", str[0]);
+			ft_printf("Number of connected players: %d\n", str[1]);
 			if (str[0] == 0)
 				break ;
 		}
