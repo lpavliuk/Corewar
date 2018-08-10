db074e2b8ef5095e02b43442d8831313dc18ec12
kuhar_tarass <kuhar.tarass@gmail.com>
2018-08-10 11:53:44 +0300

    rm vm_champs

--- vm_champs/ex.s
+++
@@ -1,8 +0,0 @@
-.name "
-tralalala"
-.comment "coool!"
-		
-l2:	sti r1,%:live,%0
-	and	:live,%0,r1
-live:	live %0
-	zjmp	%:live
--- vm_champs/zork.s
+++
@@ -1,6 +0,0 @@
-.name "zork_orig"
-.comment "I'M ALIIIIVE"
-
-
-l2:		sti r1, %:live, %1
-
